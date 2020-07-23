19cc2a5e36e8f522cff00411443f93c769d56ad2
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-07-23 23:28:50 +0545

    react-portfolio

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "./components/Cards";
+import Coding from "./components/Coding.png";
+import "./App.css";
+
+function App() {
+  const name = "Sparsh";
+
+  return (
+    <div>
+      <header className="header">
+        <h2>
+          codewith<b className="name-color">sparsh</b>
+        </h2>
+        <div className="end-header">
+          <h2 className="header-content">
+            <b>Home</b>
+          </h2>
+          <h2 className="header-content">
+            <b>About</b>
+          </h2>
+          <h2 className="header-content">
+            <b>Blog</b>
+          </h2>
+        </div>
+      </header>
+      <hr />
+      <div className="App">
+        <Card />
+      </div>
+      <h2 className="name">I'm {name}</h2>
+      <div className="content">
+        <img src={Coding} alt="" />
+        <h2>
+          Aspiring <div className="color-web">Web Developer</div>
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Cards.css";
+
+function Card() {
+  return (
+    <div className="card">
+      <h2>Hello World!</h2>
+    </div>
+  );
+}
+
+export default Card;
